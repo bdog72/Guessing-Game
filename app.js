6bd55a078e01e21e5570186328c6dac40cdc0d7e
Brian Bycynski <high5bri@hotmail.com>
2018-06-26 06:15:27 -0400

    finished lecture 50

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('Bozo');
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. Random Number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display The Result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = './images/dice-' + dice + '.png';
+
+  // 3. Update The Round Score, if the rolled number was Not a 1
+  if (dice !== 1) {
+    // Add Score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    // Next Player
+    activePlayer === 0 ? (activePlayer = 1) : activePlayer === 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+    document.querySelector('.dice').style.display = 'none';
+  }
+});
